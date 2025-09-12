6679ba24d39419aaa5ca94377979c64a471997f2
zeel <sundayy119@gmail.com>
2025-09-12 11:57:02 +0530

    print body from components file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NotesList from './components/NotesList';
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <NotesList />
     </div>
   );
 }
---
+++ src/components/NotesList.js
@@ -0,0 +1,11 @@
+const NotesList = () => {
+    return ( 
+        <div>
+            <h1>NotesList</h1>
+        </div>
+        
+
+     );
+}
+ 
+export default NotesList;

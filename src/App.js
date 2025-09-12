a604b0c6cc558bf381a87672c46681181258b3c8
zeel <sundayy119@gmail.com>
2025-09-12 18:47:07 +0530

    adding react routing and using it to navigate between different web pages using react route and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import NotesList from './components/NotesList';
+import Navbar from "./components/Navbar";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <div>
-      <NotesList />
-    </div>
+    <BrowserRouter>
+        <Routes>
+            
+            <Route path="/*" element={<Navbar />} />
+            <Route path="/" element={<NotesList /> } />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    
+    </BrowserRouter>
   );
 }
 
